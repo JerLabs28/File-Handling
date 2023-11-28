7090b22bb18251fe7ffa5cdf9a5a9f73c99d1033
JerLabs28 <151612231+JerLabs28@users.noreply.github.com>
2023-11-28 15:24:58 +0800

    Add files via upload

---
+++ LABSKERT/src/main/java/com/mycompany/labskert/LABSKERT.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.labskert;
+
+/**
+ *
+ * @author WS
+ */
+public class LABSKERT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
